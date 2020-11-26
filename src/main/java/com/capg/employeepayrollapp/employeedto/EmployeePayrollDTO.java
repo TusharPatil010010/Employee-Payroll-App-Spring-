0fc3b140f277359f261de139c6d0e55abec20142
TusharPatil010010 <tusharp010010@gmail.com>
2020-11-26 13:29:28 +0530

    Added : added the employee payroll app spring data

---
+++ src/main/java/com/capg/employeepayrollapp/employeedto/EmployeePayrollDTO.java
@@ -0,0 +1,11 @@
+package com.capg.employeepayrollapp.employeedto;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class EmployeePayrollDTO {
+	@NotEmpty(message = "Name should not be null")
+	public String name;
+	@NotNull
+	public long salary;
+}
